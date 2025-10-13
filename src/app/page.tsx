4af2434c9f5d09ac7223e0b6ac676373b9c6e961
Dhanush R <dhanuraj7258@gmail.com>
2025-10-13 22:33:21 +0530

    feat(trpc): add tRPC setup with React Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-const page = () => {
-  return <div>page</div>;
+'use client';
+
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
+
+const Page = () => {
+  const trpc = useTRPC();
+  const { data: users } = useQuery(trpc.getUsers.queryOptions());
+  return <div>{JSON.stringify(users)}</div>;
 };
 
-export default page;
+export default Page;
