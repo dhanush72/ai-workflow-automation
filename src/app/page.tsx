c67c22345bfca78e47929fcd13ce15730a6ae7dd
Dhanush R <dhanuraj7258@gmail.com>
2025-10-19 16:22:22 +0530

    feat(theme): add theme provider and toggle components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { requireAuth } from '@/lib/auth-session';
 import { LogoutBtn } from '@/lib/logout';
 import { caller } from '@/trpc/server';
+import Image from 'next/image';
 
 const Page = async () => {
   await requireAuth();
@@ -9,6 +10,7 @@ const Page = async () => {
   console.log({ data });
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image src="/logo.svg" alt="logo" width={100} height={100} />
       <p>You are logged in!</p>
       <div>{JSON.stringify(data, null, 2)}</div>
       <LogoutBtn />
